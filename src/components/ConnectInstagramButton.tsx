97f47c4e93fa745ebc575ccaacc97caf9d2ddb3f
Jay-kanani18 <jkanani.elluminati@gmail.com>
2026-03-06 18:55:29 +0530

    update insta

--- src/components/ConnectInstagramButton.tsx
+++ src/components/ConnectInstagramButton.tsx
@@ -10,10 +10,13 @@ const scopes = [
   // "instagram_manage_messages",
   // "pages_show_list",
   // "pages_manage_metadata"
-  "pages_messaging",
+  // "pages_messaging",
 
 "instagram_basic",  
-"pages_manage_metadata"
+"instagram_business_manage_messages",
+"instagram_manage_messages"
+
+// "pages_manage_metadata"
   // "business_management", "whatsapp_business_management", "whatsapp_business_messaging", "public_profile"
 ].join(",");
 
