b5979313572215ffbb984eb1417c589100310dfb
JAY-KANANI-18 <jaykanani1999@gmail.com>
2026-04-05 18:33:09 +0530

    socker debug

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -5,7 +5,8 @@ let socket: Socket | null = null;
 const SOCKET_BASE =  import.meta.env.VITE_SOCKET_URL || "http://localhost:3000/api";
 
 export function connectSocket(token: string): Socket {
-
+  console.log({SOCKET_BASE,token,socket});
+  
   if (socket) return socket;   // prevents duplicate connections
 
   socket = io(`${SOCKET_BASE}/inbox`, {
