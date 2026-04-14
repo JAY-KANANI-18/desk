38c4bf13f9807606a0dfdcdcfe9a40496cfc814d
JAY-KANANI-18 <jaykanani1999@gmail.com>
2026-04-14 17:55:55 +0530

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,17 @@
-import { Outlet, matchPath, useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import { AppSidebar } from './AppSidebar';
-import { TopBar } from './TopBar';
-import { NotificationListWrapper } from './NotificationList';
-import { IncomingCallWindow } from './IncomingCallWindow';
-import { ActiveCallWindow } from './ActiveCallWindow';
-import { MobileBottomNav } from './MobileBottomNav';
+import { Outlet, matchPath, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { AppSidebar } from "./AppSidebar";
+import { TopBar } from "./TopBar";
+import { NotificationListWrapper } from "./NotificationList";
+import { IncomingCallWindow } from "./IncomingCallWindow";
+import { ActiveCallWindow } from "./ActiveCallWindow";
+import { MobileBottomNav } from "./MobileBottomNav";
 
 export const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const location = useLocation();
   const hideTopBarOnMobile = Boolean(
-    matchPath('/inbox/:conversationId', location.pathname),
+    matchPath("/inbox/:conversationId", location.pathname),
   );
 
   return (
@@ -30,22 +30,26 @@ export const Layout = () => {
       <div
         className={`fixed inset-y-3 left-3 z-50 transition-all duration-300 md:hidden ${
           sidebarOpen
-            ? 'translate-x-0 opacity-100'
-            : '-translate-x-[calc(100%+1.5rem)] opacity-0'
+            ? "translate-x-0 opacity-100"
+            : "-translate-x-[calc(100%+1.5rem)] opacity-0"
         }`}
       >
         <AppSidebar variant="mobile" onNavigate={() => setSidebarOpen(false)} />
       </div>
 
       <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
-        <div className={hideTopBarOnMobile ? 'hidden md:block' : 'block'}>
+        <div className={hideTopBarOnMobile ? "hidden md:block" : "block"}>
           <TopBar onOpenSidebar={() => setSidebarOpen(true)} />
         </div>
-     <main className="flex min-h-0 flex-1 flex-col overflow-hidden">
-  <div className="flex min-h-0 flex-1 flex-col pb-[4.5rem] md:pb-0">
-    <Outlet />
-  </div>
-</main>
+        <main className="flex min-h-0 flex-1 flex-col overflow-hidden">
+          <div
+            className={`flex min-h-0 flex-1 flex-col ${
+              hideTopBarOnMobile ? "" : "pb-[4.5rem] md:pb-0" 
+            }`}
+          >
+            <Outlet />
+          </div>
+        </main>
         <MobileBottomNav />
       </div>
 
